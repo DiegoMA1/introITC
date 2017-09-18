fde5bcfc7c4642551f21ede042056f9f002480e3
Diego Moreno <A01022113@itesm.mx>
2017-09-18 12:31:18 -0500

    Otra manera

--- For1.java
+++ For1.java
@@ -1,7 +1,8 @@
 public class For1{
     public static void main (String [] args){
-        for(int cont=1;cont<=10;cont++){
+        for(int cont=1;cont<=10;){
             System.out.printf("%d ",cont);
+            cont++;
         }
     }
 }
