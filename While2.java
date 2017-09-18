fa88e25e76241b260095b9852fcf10adbe27f953
Diego Moreno <A01022113@itesm.mx>
2017-09-18 12:20:34 -0500

    Segunda version

--- While2.java
+++ While2.java
@@ -2,16 +2,17 @@ import java.util.*;
 public class While2{
     public static void main (String [] args ){
         double lado;
-        String respuesta="si";
+        String respuesta="no";
         Scanner lector = new Scanner (System.in);
-        while(respuesta.equalsIgnoreCase("Si")){
+        do{
             System.out.println("Dame el lado de un cuadrado para calcular su área");
             lado= lector.nextDouble();
             lector.nextLine();
 
             System.out.printf("El area es %.2f %n",lado*lado);
-            System.out.println("Quires hacerlo de nuevo");
+            System.out.println("Quieres hacerlo de nuevo");
             respuesta=lector.nextLine();
         }
+        while(!respuesta.equalsIgnoreCase("No"));
     }
 }
